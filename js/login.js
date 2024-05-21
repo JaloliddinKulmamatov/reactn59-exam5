abaa7ae075eb9dc7673ec13005950f106e7f3dbb
JaloliddinKulmamatov <itjalol2010@gmail.com>
2024-05-21 17:20:08 +0500

    jalol

--- js/login.js
+++ js/login.js
@@ -79,7 +79,7 @@ document.addEventListener("DOMContentLoaded", () => {
         const token = localStorage.getItem("token");
 
         if (token) {
-            window.location.href = "http://127.0.0.1:5500/index.html";
+            window.location.href = "/index.html";
         }
     }
 
